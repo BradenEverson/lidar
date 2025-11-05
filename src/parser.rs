fa2dac84376bb5775dedfcdb9d8942cad5681d59
BradenEverson <bradeneverson@gmail.com>
2025-11-05 11:13:56 +0000

    Wow that actually worked

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,7 @@ pub struct ResponseParser {
     curr: FlatResponse,
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct FlatResponse {
     response_len: u32,
     send_mode: u8,
@@ -38,10 +38,10 @@ impl ResponseParser {
             }
 
             (ParserState::ReadingDataLenSendMode(step), b) => {
-                self.curr.response_len |= (b as u32) << ((3 - step) * 8);
+                self.curr.response_len |= (b as u32) << ((step) * 8);
                 if step == 3 {
-                    self.curr.send_mode = (self.curr.response_len & 3) as u8;
-                    self.curr.response_len &= !3;
+                    self.curr.send_mode = (b >> 6) & 3;
+                    self.curr.response_len &= 0x3FFFFFFF;
                     self.state = ParserState::ReadingDataType
                 } else {
                     self.state = ParserState::ReadingDataLenSendMode(step + 1)
@@ -50,6 +50,7 @@ impl ResponseParser {
 
             (ParserState::ReadingDataType, d) => {
                 self.curr.dtype = d;
+                self.state = ParserState::ReadingPayload;
             }
 
             (ParserState::ReadingPayload, p) => {
@@ -76,4 +77,28 @@ pub enum ParserState {
     ReadingDataLenSendMode(u8),
     ReadingDataType,
     ReadingPayload,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{FlatResponse, ResponseParser};
+
+    #[test]
+    fn simple_response_descriptor() {
+        let scan_resp = [
+            0xA5, 0x5A, 0x05, 0x00, 0x00, 0x40, 0x81, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        let expected = FlatResponse {
+            dtype: 0x81,
+            send_mode: 0x01,
+            response_len: 5,
+            payload: vec![0, 0, 0, 0, 0],
+        };
+
+        let mut parser = ResponseParser::default();
+        let responses = parser.feed(&scan_resp);
+
+        assert_eq!(responses[0], expected)
+    }
 }
